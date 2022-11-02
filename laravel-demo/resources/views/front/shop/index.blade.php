23258c5dda426c5a65e2d1ac3fe72f4ab87f5f75
Nguyễn Quang Huy <643007@sv.vnua.edu.vn>
2022-11-02 15:50:24 +0700

    update shop

--- laravel-demo/resources/views/front/shop/index.blade.php
+++ laravel-demo/resources/views/front/shop/index.blade.php
@@ -3,5 +3,41 @@
 @section('title', 'Shop')
 
 @section('body')
-   
+   <div class="breacrumb-section"></div>
+
+   <section class="product-shop spad">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-8 order-2 order-lg-1 products-sidebar-filter"></div>
+        <div class="col-lg-9 order-1 order-lg-2">
+          <div class="product-show-option"></div>
+          <div class="product-list">
+            <div class="row">
+              <div class="col-lg-4 col-sm-6">
+                <div class="product-item">
+                  <div class="pi-pic">
+                    <img src="../../../../public/front/img/products/1.1.jpg" alt="">
+                    <div class="sale pp-sale">Sale</div>
+                    <div class="icon"><i class="icon_heart_alt"></i></div>
+                    <ul>
+                      <li class="w-icon active"><a href=""><i class="icon_bag_alt"></i></a></li>
+                      <li class="quick-view"><a href="">+ Quick View</a></li>
+                      <!-- <li class="w-icon"><a href=""><i class="fa fa-random"></a></li> -->
+                    </ul>
+                  </div>
+                  <div class="pi-text">
+                    <div class="category-name">Towel</div>
+                    <a href="">
+                      <h5>Pure Pineapple</h5>
+                    </a>
+                    <div class="product-price">$14.00 <span>$35</span></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   </section>
 @endsection
