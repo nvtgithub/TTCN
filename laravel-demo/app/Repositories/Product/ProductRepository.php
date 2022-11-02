c212b61d17469dfccb46039d9b81dd49a0ade4ca
Nguyễn Quang Huy <643007@sv.vnua.edu.vn>
2022-11-02 16:49:17 +0700

    fix 23 search

--- laravel-demo/app/Repositories/Product/ProductRepository.php
+++ laravel-demo/app/Repositories/Product/ProductRepository.php
@@ -8,23 +8,60 @@ use App\Models\Product;
 class ProductRepository extends BaseRepository implements ProductRepositoryInterface
 {
 
-    public function getModel()
-    {
-        return Product::class;
+  public function getModel()
+  {
+    return Product::class;
+  }
+
+  public function getRelatedProducts($product, $limit = 4)
+  {
+    return $this->model->where('product_category_id', $product->product_category_id)
+      ->where('tag', $product->tag)
+      ->limit($limit)
+      ->get();
+  }
+
+  public function getFeatureProductByCategory(int $categoryId)
+  {
+    return $this->model->where('featured', true)
+      ->where('product_category_id', $categoryId)
+      ->get();
+  }
+
+  public function getProductOnIndex($request)
+  {
+    $perPage = $request->show ?? 3;
+    $sortBy = $request->sort_by ?? 'Latest';
+    $search = $request->search ?? '';
+    $products = $this->model->where('name', 'like', '%' . $search . '%');
+
+    switch ($sortBy) {
+      case 'latest':
+        $products = $products->orderBy('id');
+        break;
+      case 'oldest':
+        $products = $products->orderByDesc('id');
+        break;
+      case 'name-ascending':
+        $products = $products->orderBy('name');
+        break;
+      case 'name-descending':
+        $products = $products->orderByDesc('name');
+        break;
+      case 'price-ascending':
+        $products = $products->orderBy('price');
+        break;
+      case 'price-descending':
+        $products = $products->orderByDesc('price');
+        break;
+
+      default:
+        $products = $products->orderBy('id');
+        break;
     }
 
-    public function getRelatedProducts($product, $limit = 4)
-    {
-        return $this->model->where('product_category_id', $product->product_category_id)
-            ->where('tag', $product->tag)
-            ->limit($limit)
-            ->get();
-    }
-
-    public function getFeatureProductByCategory(int $categoryId)
-    {
-        return $this->model->where('featured', true)
-            ->where('product_category_id', $categoryId)
-            ->get();
-    }
+    $products = $products->paginate($perPage);
+    $products->appends(['sort_by' => $sortBy, 'show' => $perPage]);
+    return $products;
+  }
 }
