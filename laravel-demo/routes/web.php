aeb9875c41681c977ce1f2669028d56b36ca4120
Nguyễn Quang Huy <643007@sv.vnua.edu.vn>
2022-12-08 18:45:56 +0700

    thêm màu sản phẩm, cập nhật số lượng

--- laravel-demo/routes/web.php
+++ laravel-demo/routes/web.php
@@ -61,7 +61,7 @@ Route::prefix('account')->group(function () {
   });
 
   Route::prefix('my-contact')->middleware('CheckMemberLogin')->group(function () {
-      Route::resource('contactuser', App\Http\Controllers\Front\ContactUserController::class);
+    Route::resource('contactuser', App\Http\Controllers\Front\ContactUserController::class);
   });
 });
 
@@ -78,9 +78,14 @@ Route::prefix('admin')->middleware('CheckAdminLogin')->group(function () {
   Route::resource('order', App\Http\Controllers\Admin\OrderController::class);
   Route::resource('revenu_day', App\Http\Controllers\Admin\RevenusDayController::class);
   Route::resource('revenu_month', App\Http\Controllers\Admin\RevenusMonthController::class);
+  // Route::resource('revenu_filter_day', App\Http\Controllers\Admin\RevenusFilterDayController::class);
+  // Route::post('revenu_filter_day/filter_by_date', [App\Http\Controllers\Admin\RevenusFilterDayController::class], 'filter_by_date');
   Route::prefix('revenu_filter_day')->group(function () {
     Route::get('/', [App\Http\Controllers\Admin\RevenusFilterDayController::class, 'index']);
   });
+
+  // Route::post('order_{{id}}', [App\Http\Controllers\Admin\OrderController::class, 'Confirmed']);
+
   Route::prefix('login')->group(function () {
     Route::get('', [\App\Http\Controllers\Admin\HomeController::class, 'getLogin'])->withoutMiddleware('CheckAdminLogin');
     Route::post('', [App\Http\Controllers\Admin\HomeController::class, 'postLogin'])->withoutMiddleware('CheckAdminLogin');
