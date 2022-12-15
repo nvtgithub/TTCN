358411513cb92a76b286be03011ee90bb31469b0
joomaio.thinh <minhthinh06112001@gmail.com>
2022-12-15 21:38:40 +0700

    update ui edit color

--- laravel-demo/resources/views/admin/product/detail/edit.blade.php
+++ laravel-demo/resources/views/admin/product/detail/edit.blade.php
@@ -20,64 +20,63 @@
         </div>
       </div>
     </div>
+  </div>
 
-    <div class="row">
-      <div class="col-md-12">
-        <div class="main-card mb-3 card">
-          <div class="card-body">
-            <form method="post" action="admin/product/{{ $product->id }}/detail/{{ $productDetail->id }}" enctype="multipart/form-data">
-              @csrf
-              @method('PUT')
-              <div class="position-relative row form-group">
-                <label class="col-md-3 text-md-right col-form-label">Tên sản phẩm</label>
-                <div class="col-md-9 col-xl-8">
-                  <input disabled placeholder="Product Name" type="text" class="form-control" value="{{ $product->name }}">
-                </div>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="main-card mb-3 card">
+        <div class="card-body">
+          <form method="post" action="admin/product/{{ $product->id }}/detail/{{ $productDetail->id }}" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="position-relative row form-group">
+              <label class="col-md-3 text-md-right col-form-label">Tên sản phẩm</label>
+              <div class="col-md-9 col-xl-8">
+                <input disabled placeholder="Product Name" type="text" class="form-control" value="{{ $product->name }}">
               </div>
+            </div>
 
-              <div class="position-relative row form-group">
-                <label for="color" class="col-md-3 text-md-right col-form-label">Màu sắc</label>
-                <div class="col-md-9 col-xl-8">
-                  <input required name="color" id="color" placeholder="Color" type="text" class="form-control" value="{{ $productDetail->color }}">
-                </div>
+            <div class="position-relative row form-group">
+              <label for="color" class="col-md-3 text-md-right col-form-label">Màu sắc</label>
+              <div class="col-md-9 col-xl-8">
+                <input required name="color" id="color" placeholder="Color" type="text" class="form-control" value="{{ $productDetail->color }}">
               </div>
+            </div>
 
-              <div class="position-relative row form-group">
-                <label for="color" class="col-md-3 text-md-right col-form-label">Mã màu</label>
-                <div class="col-md-9 col-xl-8">
-                  <input type="color" class="form-control-color" id="color" name="color_code" value="{{ $productDetail->color_code }}" title="Chọn 1 màu">
-                </div>
+            <div class="position-relative row form-group">
+              <label for="color" class="col-md-3 text-md-right col-form-label">Mã màu</label>
+              <div class="col-md-9 col-xl-8">
+                <input type="color" class="form-control-color" id="color" name="color_code" value="{{ $productDetail->color_code }}" title="Chọn 1 màu">
               </div>
+            </div>
 
-              <div class="position-relative row form-group">
-                <label for="qty" class="col-md-3 text-md-right col-form-label">Số lượng</label>
-                <div class="col-md-9 col-xl-8">
-                  <input required name="qty" id="qty" placeholder="Size" type="text" class="form-control" value="{{ $productDetail->qty }}">
-                </div>
+            <div class="position-relative row form-group">
+              <label for="qty" class="col-md-3 text-md-right col-form-label">Số lượng</label>
+              <div class="col-md-9 col-xl-8">
+                <input required name="qty" id="qty" placeholder="Size" type="text" class="form-control" value="{{ $productDetail->qty }}">
               </div>
+            </div>
 
-              <div class="position-relative row form-group mb-1">
-                <div class="col-md-9 col-xl-8 offset-md-2">
-                  <a href="./admin/product/{{ $product->id }}/detail" class="border-0 btn btn-outline-danger mr-1">
-                    <span class="btn-icon-wrapper pr-1 opacity-8">
-                      <i class="fa fa-times fa-w-20"></i>
-                    </span>
-                    <span>Hủy</span>
-                  </a>
+            <div class="position-relative row form-group mb-1 d-flex justify-content-center">
+              <a href="./admin/product/{{ $product->id }}/detail" class="border-0 btn btn-outline-danger mr-1">
+                <span class="btn-icon-wrapper pr-1 opacity-8">
+                  <i class="fa fa-times fa-w-20"></i>
+                </span>
+                <span>Hủy</span>
+              </a>
 
-                  <button type="submit" class="btn-shadow btn-hover-shine btn btn-primary">
-                    <span class="btn-icon-wrapper pr-2 opacity-8">
-                      <i class="fa fa-save fa-w-20"></i>
-                    </span>
-                    <span>Lưu</span>
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
+              <button type="submit" class="btn-shadow btn-hover-shine btn btn-primary">
+                <span class="btn-icon-wrapper pr-2 opacity-8">
+                  <i class="fa fa-save fa-w-20"></i>
+                </span>
+                <span>Lưu</span>
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
   </div>
-  <!-- End Main -->
-  @endsection
+</div>
+<!-- End Main -->
+@endsection
